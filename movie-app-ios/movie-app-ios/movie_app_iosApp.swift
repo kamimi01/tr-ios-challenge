219e1b66907f0544ab492f0abffe7604ac5537d0
kamimi01 <awakaru0122@gmail.com>
2025-08-26 18:59:28 +0200

    feat: init a project

---
+++ movie-app-ios/movie-app-ios/movie_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_iosApp.swift
+//  movie-app-ios
+//
+//  Created by mika_admin on 2025-08-26.
+//
+
+import SwiftUI
+
+@main
+struct movie_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ movie-app-ios/movie-app-iosUITests/movie_app_iosUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  movie_app_iosUITestsLaunchTests.swift
+//  movie-app-iosUITests
+//
+//  Created by mika_admin on 2025-08-26.
+//
+
+import XCTest
+
+final class movie_app_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
